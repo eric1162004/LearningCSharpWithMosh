bbb4018fb9fb1da5bc21c734c0070f717b666926
Eric Cheung <000437017@student.vcc.ca>
2021-03-11 23:37:28 -0800

    Async

---
+++ Dynamic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Mosh";
+            // obj.GetHashCode();
+            
+            // Using Reflection
+            var methodInfo = obj.GetType().GetMethod("GetHarshCode");
+            // methodInfo.Invoke(null);
+
+            // Using dynamic
+            dynamic excelObject = "mosh";
+            // excelObject.Optimal();
+
+            // Type resolve at runtime and can be changed to other type
+            dynamic name = "Mosh";
+            name = 6;
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+
+
+        }
+    }
+}
